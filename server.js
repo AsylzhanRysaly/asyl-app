25ccafbcd59fc91ae1c10643dbec62fe0e565b9c
AsylzhanRysaly <assiljan@mail.ru>
2016-11-27 23:09:52 +0600

    correct massage

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 3000;
 
 var cars = [];
 app.get('/', function(req, res){
-  res.send('Қош келдіңіз!!! \n GET сұраныс жасау үшін');
+  res.send('Қош келдіңіз!!! \n GET сұраныс жасау үшін /cars \n POST  жасау үшін /cars  \n DELETE сұраныс жасау үшін /cars');
 });
 
 app.get('/cars', function(req, res) {
